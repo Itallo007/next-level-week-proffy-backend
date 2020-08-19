74ae7774ee21102aae967b19d5e384970f7438b7
itallo007 <italosantos.oficial2000@gmail.com>
2020-08-19 20:28:12 -0300

    Modifica a tabela users no bando de dados
    os campos lastName, email e password foram adicionados

--- src/database/migrations/00_create_users.ts
+++ src/database/migrations/00_create_users.ts
@@ -1,10 +1,12 @@
-import { table } from "console";
 import Knex from "knex";
 
 export async function up(knex: Knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id').primary();
     table.string('name').notNullable();
+    table.string('lastName').notNullable();
+    table.string('email').notNullable();
+    table.string('password').notNullable();
     table.string('avatar').notNullable();
     table.string('whatsapp').notNullable();
     table.string('bio').notNullable();
