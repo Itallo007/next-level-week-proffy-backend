1d383fa97e8df3e5a7f1cf952fa97c9372f185a8
itallo007 <italosantos.oficial2000@gmail.com>
2020-08-25 16:55:13 -0300

    Implementa o método findOne (user)
    o método busca um usuário pelo email
    o método recebe o email pela requisição, valída e busca no banco
    o método retorna um user como json

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from "express";
 import db from "../database/connection";
+import * as EmailValidator from "email-validator";
 
 
 export default class UserController {
@@ -36,5 +37,29 @@ export default class UserController {
         message: "Unexpected error while creating new user"
       });
     }
+  }
+
+  async findOne(req: Request, res:Response) {
+    const {email} = req.body;
+
+    if(!EmailValidator.validate(email)) {
+      return res.status(400).json({message: "Invalid email"});
+    }
+
+    try {
+      const userFound = await db('users').where({email});
+
+      if(!userFound) {
+        return res.status(400).json({message: "User not found"});
+      }
+
+      return res.status(200).json(userFound);
+
+    } catch (error) {
+      return res.status(400).json({
+        error
+      });
+    }
+    
   }
 }
